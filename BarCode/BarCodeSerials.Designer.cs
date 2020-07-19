2adceb853a6c117f0f7c16eaa6cb91298e1f02a5
MinaSamir11 <menasamer11@gmail.com>
2020-07-19 06:39:02 +0200

    Desktop app, generate unique serial number and lock file of serials no one can access file without app

---
+++ BarCode/BarCodeSerials.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace BarCode
+{
+    partial class BarCodeSerials
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(78, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Genrate Serial";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 21F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Serial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(348, 54);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Genrate Serials";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(497, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(2, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Eng\\MinaSamir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(3, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "01117558274";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(3, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "menasamer11@gmail.com";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(363, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(209, 308);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(98, 151);
+            this.textBox1.MaxLength = 3275;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(134, 26);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BarCodeSerials
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(609, 395);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Orange;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "BarCodeSerials";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.BarCodeSerials_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BarCode/BarCodeSerials.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using static System.Math;
+using System.Drawing;
+using System.Security.AccessControl;
+
+namespace BarCode
+{
+    public partial class BarCodeSerials : Form
+    {
+        public BarCodeSerials()
+        {
+            InitializeComponent();
+        }
+      
+  
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            DialogResult result1 = MessageBox.Show("Do you Want To Create New Serial",
+                    "request serial",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+            if (result1 != DialogResult.Yes)
+                return;
+            else
+            {
+                String FileName = "Serials";
+                String Path = @"D:\Genrate Serial\Data\";
+                if (Directory.Exists(Path))
+                {
+                    try
+                    {
+                        string folderPath = Path;
+                        string adminUserName = Environment.UserName;// getting your adminUserName
+                        DirectorySecurity ds = Directory.GetAccessControl(folderPath);
+                        FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny);
+                        ds.AddAccessRule(fsa);
+                        Directory.SetAccessControl(folderPath, ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+                }
+                    if (!Directory.Exists(Path))
+                {
+                    Directory.CreateDirectory(Path);
+                    StreamWriter stream = new StreamWriter(Path+FileName);
+                    File.SetAttributes(Path + FileName, File.GetAttributes(Path + FileName) | FileAttributes.Hidden);
+
+                    try
+                    {
+                        string folderPath = Path;
+                        string adminUserName = Environment.UserName;// getting your adminUserName
+                        DirectorySecurity ds = Directory.GetAccessControl(folderPath);
+                        FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny);
+                        ds.AddAccessRule(fsa);
+                        Directory.SetAccessControl(folderPath, ds);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    stream.Close();
+
+     //               unlock folder
+     //               try
+     //               {
+     //                   string folderPath = textBox1.Text;
+     //                   string adminUserName = Environment.UserName;// getting your adminUserName
+     //                   DirectorySecurity ds = Directory.GetAccessControl(folderPath);
+     //                   FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny)
+     //ds.RemoveAccessRule(fsa);
+     //                   Directory.SetAccessControl(folderPath, ds);
+     //                   MessageBox.Show("UnLocked");
+     //               }
+     //               catch (Exception ex)
+     //               {
+     //                   MessageBox.Show(ex.Message);
+     //               }
+                }
+             
+                StreamReader reader = new StreamReader(Path + FileName);
+              
+                String Content = reader.ReadToEnd();
+                reader.Close();
+                Boolean temp = false;
+                Random r = new Random();
+                while (!temp)
+                {
+                    int randomnum = r.Next();
+                    if (!Content.Contains(randomnum.ToString() + "\n"))
+                    {
+                        StreamWriter writer = new StreamWriter(Path + FileName, true);
+                        writer.WriteLine(randomnum.ToString() + "\n");
+                        writer.Close();
+                        label1.Text = randomnum.ToString();
+                        richTextBox1.Text = Content + randomnum.ToString();
+                        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        // scroll it automatically
+                        richTextBox1.ScrollToCaret();
+                        textBox1.Text = randomnum.ToString();
+                        textBox1.Focus();
+                        temp = true;
+                    }
+                }
+            }
+        }
+
+        private void BarCodeSerials_Load(object sender, EventArgs e)
+        {
+            this.Icon = Icon.ExtractAssociatedIcon(AppDomain.CurrentDomain.FriendlyName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
